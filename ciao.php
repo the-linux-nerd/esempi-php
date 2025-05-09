754608658e6bffff72edae3018840f63a1a4fd32
Team Produzione Istrice <produzione@istricesrl.it>
2025-05-09 17:31:23 +0200

    prima lezione

---
+++ ciao.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "<html>" . PHP_EOL;
+    echo "<head>" . PHP_EOL;
+    echo "<title>ciao</title>" . PHP_EOL;
+    echo "<link rel='stylesheet' href='style.css'>" . PHP_EOL;
+    echo "</head>" . PHP_EOL;
+    echo "<body>" . PHP_EOL;
+    echo "<h1>ciao</h1>" . PHP_EOL;
+    echo "<p>ciao</p>" . PHP_EOL;
+    echo "</body>" . PHP_EOL;
+    echo "</html>" . PHP_EOL;
---
+++ variabili.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a = 10;
+
+    $t = "ciao";
+
+    echo $a . "<br>" . PHP_EOL;
+    echo $t . "<br>" . PHP_EOL;
+
+    $b = 20;
+
+    $c = $a + $b;
+    $d = $a * $b;
+    $e = $a / $b;
+    $f = $a - $b;
+    $g = $a % $b;
+
+    echo "$c è la somma di $a e $b<br>" . PHP_EOL;
+    echo '$c è la somma di $a e $b<br>' . PHP_EOL;
+    echo $c . ' è la somma di ' . $a . ' e ' . $b . '<br>' . PHP_EOL;
